c7b8b783d9bef2c8d4dc280b17d8d28686f7f5f1
Nitesh <nitesh.mhapankar@concertosoft.com>
2018-11-13 18:47:55 +0530

    Login Functionality Added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError , DataRequired ,EqualTo ,Email
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired() ,Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Reapeat Password', validators=[DataRequired(),EqualTo('password')])
+	submit = SubmitField('Sign In')
+
+
+	def validate_username(self , username):
+		user =User.query.filter_by(username = username.data).first()
+		if user is not None:
+			raise ValidationError("Please Select Different Username")
+
+	def validate_email(self , email):
+		user =User.query.filter_by(email = email.data).first()
+		if user is not None:
+			raise ValidationError("Please Select Different Email Address")
