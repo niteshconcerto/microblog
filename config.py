3ca31388844fdb7fd8b1a2ade8385e94dff5908a
Nitesh <nitesh.mhapankar@concertosoft.com>
2018-11-14 17:17:03 +0530

    Exception Handling And Logs

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = "SMTP01.concertosoft.com"
+    MAIL_PORT = 25
+    MAIL_USE_TLS = None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = []
